0350d7be078353aadd95b6237de755ad7e39f263
Nikolay Fadin <kuvaldis1988@gmail.com>
2015-03-18 23:10:03 +0300

    fix zookeeper run

--- zookeeper/run.sh
+++ zookeeper/run.sh
@@ -1,7 +1,4 @@
 docker run --name zk-kuvaldis-v1 -d \
--v /opt/zookeeper/conf:/home/kuvaldis/Work/Mine/Study/playground/zookeeper/conf \
--v /var/lib/zookeeper:/home/kuvaldis/Work/Mine/Study/tmp/zk \
--p 2181:2181
-zookeeper-kuvaldis
-
-
+-v /home/kuvaldis/Work/Mine/Study/playground/zookeeper/conf:/opt/zookeeper/conf \
+-v /home/kuvaldis/Work/Mine/Study/tmp/zk1:/var/lib/zookeeper \
+-p 2181:2181 -p 2888:2888 -p 3888:3888 zookeeper-kuvaldis
