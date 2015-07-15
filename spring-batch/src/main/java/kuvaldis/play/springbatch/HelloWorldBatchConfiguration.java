930fbd56c4d16f309335cd7750a5ce31f2c2f1e9
Nikolay Fadin <kuvaldis1988@gmail.com>
2015-07-15 15:23:05 +0300

    spring-batch hello world play

---
+++ spring-batch/src/main/java/kuvaldis/play/springbatch/HelloWorldBatchConfiguration.java
@@ -0,0 +1,50 @@
+package kuvaldis.play.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+@EnableAutoConfiguration
+public class HelloWorldBatchConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> null)
+                .build();
+    }
+
+    @Bean
+    public Job job1(Step step1) throws Exception {
+        return jobBuilderFactory.get("job1")
+                .incrementer(new RunIdIncrementer())
+                .start(step1)
+                .build();
+    }
+
+    public static void main(String[] args) {
+        System.exit(SpringApplication.exit(
+                SpringApplication.run(HelloWorldBatchConfiguration.class, args)));
+    }
+}
---
+++ spring-batch/src/main/java/kuvaldis/play/springbatch/person/PersonBatchConfiguration.java
@@ -0,0 +1,96 @@
+package kuvaldis.play.springbatch.person;
+
+import org.h2.jdbcx.JdbcDataSource;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+
+@Configuration
+@SpringBootApplication
+@EnableBatchProcessing
+@ComponentScan
+public class PersonBatchConfiguration {
+
+    @Bean
+    public LineMapper<Person> lineMapper() {
+        final DefaultLineMapper<Person> lineMapper = new DefaultLineMapper<>();
+        final DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setNames(new String[]{"firstName", "lastName"});
+        final BeanWrapperFieldSetMapper<Person> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Person.class);
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        return lineMapper;
+    }
+
+    @Bean
+    public ItemReader<Person> reader(final LineMapper<Person> lineMapper) {
+        final FlatFileItemReader<Person> reader = new FlatFileItemReader<>();
+        reader.setResource(new ClassPathResource("people.csv"));
+        reader.setLineMapper(lineMapper);
+        return reader;
+    }
+
+    @Bean
+    public ItemWriter<Person> writer(final DataSource dataSource) throws SQLException {
+        dataSource.getConnection().createStatement().execute("" +
+                "CREATE TABLE PEOPLE (" +
+                "  FIRST_NAME varchar(255) NOT NULL," +
+                "  LAST_NAME varchar(255) NOT NULL" +
+                ");");
+        JdbcBatchItemWriter<Person> writer = new JdbcBatchItemWriter<>();
+        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
+        writer.setSql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)");
+        writer.setDataSource(dataSource);
+        return writer;
+    }
+
+    @Bean
+    public Job importUserJob(final JobBuilderFactory jobBuilderFactory, final Step step1, final JobExecutionListener jobExecutionListener) {
+        return jobBuilderFactory.get("importUserJob")
+                .incrementer(new RunIdIncrementer())
+                .listener(jobExecutionListener)
+                .flow(step1)
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1(final StepBuilderFactory stepBuilderFactory, final ItemReader<Person> reader,
+                      final ItemWriter<Person> writer, final ItemProcessor<Person, Person> processor) {
+        return stepBuilderFactory.get("step1")
+                .<Person, Person>chunk(10)
+                .reader(reader)
+                .processor(processor)
+                .writer(writer)
+                .build();
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(PersonBatchConfiguration.class, args);
+    }
+}
