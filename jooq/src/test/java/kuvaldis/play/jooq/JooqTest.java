5e97868342df4d325e607b86f39067a7b9aae59a
kuvaldis <kuvaldis1988@gmail.com>
2015-08-06 01:02:15 +0300

    #66 jooq configuration test

--- jooq/src/test/java/kuvaldis/play/jooq/JooqTest.java
+++ jooq/src/test/java/kuvaldis/play/jooq/JooqTest.java
@@ -2,12 +2,18 @@ package kuvaldis.play.jooq;
 
 import kuvaldis.play.jooq.generated.public_.tables.Book;
 import org.jooq.*;
+import org.jooq.exception.DataAccessException;
 import org.jooq.impl.DSL;
+import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.DefaultExecuteListener;
+import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.junit.Test;
 
 import java.sql.Connection;
 
 import static junit.framework.Assert.assertEquals;
+import static kuvaldis.play.jooq.DbUtils.connection;
+import static kuvaldis.play.jooq.DbUtils.context;
 import static kuvaldis.play.jooq.generated.public_.tables.Author.AUTHOR;
 import static kuvaldis.play.jooq.generated.public_.tables.Book.BOOK;
 import static org.jooq.impl.DSL.concat;
@@ -17,7 +23,7 @@ public class JooqTest {
 
     @Test
     public void testAuthorsList() throws Exception {
-        final DSLContext context = DbUtils.context();
+        final DSLContext context = context();
         final Result<Record> result = context.select().from(AUTHOR).limit(3).fetch();
 
         System.out.println(result);
@@ -42,7 +48,7 @@ public class JooqTest {
 
     @Test
     public void testConcatAndTrim() throws Exception {
-        final DSLContext context = DbUtils.context();
+        final DSLContext context = context();
         final Result<Record1<String>> result = context
                 .select(concat(trim(AUTHOR.FIRST_NAME), trim(AUTHOR.LAST_NAME)))
                 .from(AUTHOR)
@@ -53,7 +59,7 @@ public class JooqTest {
 
     @Test
     public void testWhere() throws Exception {
-        final DSLContext context = DbUtils.context();
+        final DSLContext context = context();
         final Result<Record> result = context.select()
                 .from(BOOK)
                 .where(BOOK.TITLE.like("Animal%"))
@@ -61,5 +67,30 @@ public class JooqTest {
         assertEquals(1, result.size());
         final Record record = result.get(0);
         assertEquals(2, record.getValue(BOOK.ID).intValue());
+    }
+
+    @Test(expected = DataAccessException.class)
+    public void testConfiguration() throws Exception {
+        final Connection connection = connection();
+        final Configuration configuration = new DefaultConfiguration().set(connection).set(SQLDialect.H2);
+        final String noInsertsIndicator = "kuvaldis.play.jooq.no-iserts";
+        configuration.set(new DefaultExecuteListenerProvider(new DefaultExecuteListener() {
+            @Override
+            public void start(ExecuteContext ctx) {
+                if (Boolean.TRUE.equals(ctx.configuration().data(noInsertsIndicator))) {
+
+                    // If active, fail this execution, if an INSERT statement is being executed
+                    if (ctx.query() instanceof Insert) {
+                        throw new DataAccessException("No INSERT statements allowed");
+                    }
+                }
+            }
+        }));
+        configuration.data(noInsertsIndicator, Boolean.TRUE);
+        final DSLContext context = DSL.using(configuration);
+        context.insertInto(AUTHOR)
+                .columns(AUTHOR.ID, AUTHOR.LAST_NAME)
+                .values(6, "Kuvaldis")
+                .execute();
     }
 }
