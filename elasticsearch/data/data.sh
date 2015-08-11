3d8632aadabc453eab482447e04a70a14bb15046
Nikolay Fadin <kuvaldis1988@gmail.com>
2015-08-11 18:52:50 +0300

    #82 elasticsearch play with commands

--- elasticsearch/data/data.sh
+++ elasticsearch/data/data.sh
@@ -44,3 +44,23 @@ curl -XDELETE 'localhost:9200/customer/external/_query?pretty' -d '
 {
   "query": { "match": { "name": "Jane" } }
 }'
+
+# adds two documents with index 1 and 2
+curl -XPOST 'localhost:9200/customer/external/_bulk?pretty' -d '
+{"index":{"_id":"1"}}
+{"name": "John Doe" }
+{"index":{"_id":"2"}}
+{"name": "Jane Doe" }
+'
+
+# updates document with id 1 and deletes document with id 2
+curl -XPOST 'localhost:9200/customer/external/_bulk?pretty' -d '
+{"update":{"_id":"1"}}
+{"doc": { "name": "John Doe becomes Jane Doe" } }
+{"delete":{"_id":"2"}}
+'
+
+# loads from accounts.json
+curl -XPOST 'localhost:9200/bank/account/_bulk?pretty' --data-binary @accounts.json
+# prints the indexes information with head
+curl 'localhost:9200/_cat/indices?v'
