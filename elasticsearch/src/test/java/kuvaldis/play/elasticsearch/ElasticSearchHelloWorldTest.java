cfacdfacc3df741e91474664a7848c15e783cc31
Nikolay Fadin <kuvaldis1988@gmail.com>
2015-08-26 18:47:48 +0300

    #82 elasticsearch fix ports. try index

--- elasticsearch/src/test/java/kuvaldis/play/elasticsearch/ElasticSearchHelloWorldTest.java
+++ elasticsearch/src/test/java/kuvaldis/play/elasticsearch/ElasticSearchHelloWorldTest.java
@@ -1,36 +1,43 @@
 package kuvaldis.play.elasticsearch;
 
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
-import org.elasticsearch.node.Node;
-import org.junit.After;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+import java.util.Date;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+import static org.junit.Assert.assertEquals;
 
 public class ElasticSearchHelloWorldTest {
 
     private Client client;
-    private Node node;
 
     @Before
     public void setUp() throws Exception {
-        final Node node = nodeBuilder()
-                .settings(ImmutableSettings.settingsBuilder().put("http.enabled", false)).client(true)
-                .node();
-        final Client client = node.client();
-        this.node = node;
-        this.client = client;
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        node.close();
+        client = new TransportClient(ImmutableSettings.settingsBuilder()
+                .put("cluster.name", "elasticsearch").build())
+                .addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
     }
 
     @Test
-    public void testClient() throws Exception {
-
+    public void testIndexDocument() throws Exception {
+        final IndexResponse response = client.prepareIndex("twitter", "tweet", "1")
+                .setSource(jsonBuilder()
+                                .startObject()
+                                .field("user", "kimchy")
+                                .field("postDate", new Date())
+                                .field("message", "trying out Elasticsearch")
+                                .endObject()
+                )
+                .execute()
+                .actionGet();
+        assertEquals("twitter", response.getIndex());
+        assertEquals("tweet", response.getType());
+        assertEquals("1", response.getId());
     }
 }
