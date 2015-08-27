295325593917e82aaa1f50ef248826824e4093d1
Nikolay Fadin <kuvaldis1988@gmail.com>
2015-08-27 17:53:56 +0300

    #82 elasticsearch CRUD play

--- elasticsearch/src/test/java/kuvaldis/play/elasticsearch/ElasticSearchHelloWorldTest.java
+++ elasticsearch/src/test/java/kuvaldis/play/elasticsearch/ElasticSearchHelloWorldTest.java
@@ -1,6 +1,11 @@
 package kuvaldis.play.elasticsearch;
 
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
@@ -12,6 +17,7 @@ import java.util.Date;
 
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class ElasticSearchHelloWorldTest {
 
@@ -26,18 +32,51 @@ public class ElasticSearchHelloWorldTest {
 
     @Test
     public void testIndexDocument() throws Exception {
+        DeleteResponse deleteResponse = client.prepareDelete("twitter", "tweet", "1")
+                .execute()
+                .actionGet();
+        deleteResponse.isFound();
+
         final IndexResponse response = client.prepareIndex("twitter", "tweet", "1")
                 .setSource(jsonBuilder()
-                                .startObject()
-                                .field("user", "kimchy")
-                                .field("postDate", new Date())
-                                .field("message", "trying out Elasticsearch")
-                                .endObject()
-                )
+                        .startObject()
+                        .field("user", "kimchy")
+                        .field("postDate", new Date())
+                        .field("message", "trying out Elasticsearch")
+                        .endObject())
                 .execute()
                 .actionGet();
         assertEquals("twitter", response.getIndex());
         assertEquals("tweet", response.getType());
         assertEquals("1", response.getId());
+
+        client.prepareUpdate("twitter", "tweet", "1")
+                .setDoc(jsonBuilder()
+                        .startObject()
+                        .field("user", "mrbeen")
+                        .endObject())
+                .execute()
+                .actionGet();
+        final GetResponse afterUpdateGetResponse = client.prepareGet("twitter", "tweet", "1")
+                .execute()
+                .actionGet();
+        assertEquals("mrbeen", afterUpdateGetResponse.getSource().get("user"));
+
+        client.prepareDelete("twitter", "tweet", "1")
+                .execute()
+                .actionGet();
+        final GetResponse getResponse = client.prepareGet("twitter", "tweet", "1")
+                .execute()
+                .actionGet();
+        assertFalse(getResponse.isExists());
+    }
+
+    @Test
+    public void testGroupByAgeRangesAndInsideTheGroupsGroupByGenderWithAverageBalance() throws Exception {
+//        final SearchResponse response = client.prepareSearch("bank")
+//                .setTypes("account")
+//                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+//                .setQuery()
+
     }
 }
