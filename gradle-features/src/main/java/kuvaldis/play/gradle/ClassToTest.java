3e87f92960be176650668e248a65b9e5e484ceab
Nikolay Fadin <kuvaldis1988@gmail.com>
2015-10-27 13:10:46 +0300

    #55 gradle javasource, javadoc jars, checksum

--- gradle-features/src/main/java/kuvaldis/play/gradle/ClassToTest.java
+++ gradle-features/src/main/java/kuvaldis/play/gradle/ClassToTest.java
@@ -2,6 +2,11 @@ package kuvaldis.play.gradle;
 
 public class ClassToTest {
 
+    /**
+     * Sample test method
+     *
+     * @return 'Boo' string
+     */
     public String methodToTest() {
         return "Boo";
     }
