f76a55f6ebbe90b1918174db07cba17191e9066e
Kuvaldis <kuvaldis1988@gmail.com>
2016-09-09 00:09:53 +0300

    #18 maven plugin example annotations

--- maven-modules/maven-plugin/src/main/java/kuvaldis/play/mavenplugin/MyMojo.java
+++ maven-modules/maven-plugin/src/main/java/kuvaldis/play/mavenplugin/MyMojo.java
@@ -2,6 +2,9 @@ package kuvaldis.play.mavenplugin;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -9,25 +12,21 @@ import java.io.IOException;
 
 /**
  * Goal which prints passed name
- *
- * @goal print-name
- * @phase package
  */
+// You may use also: @goal print-name @phase package
+@Mojo(name = "print-name", defaultPhase = LifecyclePhase.PACKAGE)
 public class MyMojo extends AbstractMojo {
 
-    /**
-     * @parameter expression="${print.name}"
-     */
+    // or use old way definitions like so: '@parameter expression="${print.name}"'
+    @Parameter(name = "name", property = "print.name")
     private String name;
 
-    /**
-     * @parameter default-value="1"
-     */
+    @Parameter(name = "times", defaultValue = "1")
     private int times;
 
     public void execute() throws MojoExecutionException {
         for (int i = 0; i < times; i++) {
-            System.out.println("Hello, " + name + "!");
+            getLog().info("Hello, " + name + "!");
         }
     }
 }
