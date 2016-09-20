01660fc8ed556398f700be215423bddfcbb9071f
Nikolay Fadin <Nikolay_Fadin@epam.com>
2016-09-20 13:20:26 +0300

    #265: java.util.concurrent.Exchanger example

--- java-features/src/test/java/kuvaldis/play/java/Multithreading.java
+++ java-features/src/test/java/kuvaldis/play/java/Multithreading.java
@@ -1,14 +1,12 @@
 package kuvaldis.play.java;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertEquals;
 
 public class Multithreading {
 
@@ -19,6 +17,41 @@ public class Multithreading {
         final ScheduledFuture<?> scheduledFuture = scheduler.scheduleAtFixedRate(counter::incrementAndGet, 0, 2, SECONDS);
         scheduler.schedule(() -> scheduledFuture.cancel(true), 3, SECONDS);
         Thread.sleep(3000);
-        Assert.assertEquals(2, counter.get());
+        assertEquals(2, counter.get());
+    }
+
+    @Test
+    public void testExchanger() throws Exception {
+        final Exchanger<String> exchanger = new Exchanger<>();
+        final AtomicInteger counter = new AtomicInteger();
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        final Thread producer = new Thread(() -> {
+            try {
+                exchanger.exchange("Produce");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        final Thread consumer = new Thread(() -> {
+            final String consume;
+            try {
+                consume = exchanger.exchange("Consume");
+                if ("Produce".equals(consume)) {
+                    counter.incrementAndGet();
+                    latch.countDown();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        producer.start();
+        consumer.start();
+
+        latch.await(1, TimeUnit.SECONDS);
+
+        assertEquals(1, counter.get());
     }
 }
