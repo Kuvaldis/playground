8db14111e88fdceb9dc6dd3e5e08a06c7a1248f2
Kuvaldis <kuvaldis1988@gmail.com>
2017-02-27 23:28:28 +0300

    #68: add spring data access hello world

---
+++ springframework/src/test/java/kuvaldis/play/springframework/SpringDataAccessTest.java
@@ -0,0 +1,42 @@
+package kuvaldis.play.springframework;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+
+public class SpringDataAccessTest {
+
+    private static final ApplicationContext context = new ClassPathXmlApplicationContext("test-data-access-context.xml");
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        final DataSource dataSource = context.getBean(DataSource.class);
+        final Connection connection = DataSourceUtils.getConnection(dataSource);
+        connection.createStatement().execute("CREATE TABLE test (data VARCHAR(255))");
+        connection.close();
+    }
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        // given
+        final PlatformTransactionManager transactionManager = context.getBean(PlatformTransactionManager.class);
+        final DefaultTransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
+        transactionDefinition.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionDefinition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        // when
+        final TransactionStatus transaction = transactionManager.getTransaction(transactionDefinition);
+
+    }
+}
