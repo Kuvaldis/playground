d25320642a4abe583f79c73ebac0f2ef5b7b3eac
Nikolay Fadin <Nikolay_Fadin@epam.com>
2017-09-29 09:11:14 +0300

    #116: some rest-doc api play

--- spring-restdoc/src/main/java/kuvaldis/play/restdoc/RestDocController.java
+++ spring-restdoc/src/main/java/kuvaldis/play/restdoc/RestDocController.java
@@ -2,9 +2,15 @@ package kuvaldis.play.restdoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @SpringBootApplication
 @RestController
@@ -19,6 +25,40 @@ public class RestDocController {
     @GetMapping("/simple-test")
     public String simple() {
         return "simple-test";
+    }
+
+    @GetMapping("/hypermedia-links")
+    public ResponseEntity<ResourceSupport> hypermediaLinks() {
+        final ResourceSupport result = new ResourceSupport();
+        result.add(new Link("some-url", "alpha"), new Link("another-url", "bravo"));
+        if (ThreadLocalRandom.current().nextBoolean()) {
+            result.add(new Link("random-url", "random"));
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    public static class Person {
+        public Contact contact;
+    }
+
+    public static class Contact {
+        public String name;
+        public String email;
+    }
+
+    @GetMapping("/response-fields")
+    public ResponseEntity<Person> responseFields() {
+        final Person person = new Person();
+        person.contact = new Contact();
+        person.contact.name = "Vasilii Pupkin";
+        person.contact.email = "vpup@pupup.up";
+        return ResponseEntity.ok(person);
+    }
+
+    @GetMapping("/path-params/{first}/{second}")
+    public void pathParams(final @PathVariable("first") String first,
+                           final @PathVariable("second") String second) {
+        return;
     }
 
     public static void main(String[] args) {
--- spring-restdoc/src/test/java/kuvaldis/play/restdoc/RestDocTest.java
+++ spring-restdoc/src/test/java/kuvaldis/play/restdoc/RestDocTest.java
@@ -1,22 +1,38 @@
 package kuvaldis.play.restdoc;
 
+import org.hibernate.validator.internal.metadata.raw.ConstrainedField;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.hypermedia.HypermediaDocumentation;
+import org.springframework.restdocs.hypermedia.LinkDescriptor;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import java.util.List;
+
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -50,5 +66,61 @@ public class RestDocTest {
         mockMvc.perform(get("/restdoc/simple-test").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(document("simple-test"));
+    }
+
+    @Test
+    public void testHypermediaLinks() throws Exception {
+        mockMvc.perform(get("/restdoc/hypermedia-links").accept(MediaTypes.HAL_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("hypermedia-links", HypermediaDocumentation.links(HypermediaDocumentation.halLinks(),
+                        HypermediaDocumentation.linkWithRel("alpha").description("Link to the alpha resource"),
+                        HypermediaDocumentation.linkWithRel("bravo").description("Link to the bravo resource"),
+                        HypermediaDocumentation.linkWithRel("random").description("Random link").optional()
+                )));
+    }
+
+    @Test
+    public void testResponseFields() throws Exception {
+        mockMvc.perform(get("/restdoc/response-fields").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("response-fields",
+                        PayloadDocumentation.responseFields(
+                                PayloadDocumentation.fieldWithPath("contact.email")
+                                        .type(JsonFieldType.STRING)
+                                        .description("The user's email address"),
+                                PayloadDocumentation.fieldWithPath("contact.name")
+                                        .description("The user's name"))));
+    }
+
+    @Test
+    public void testPathParams() throws Exception {
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/restdoc/path-params/{first}/{second}", "hi", "there")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("path-params", RequestDocumentation.pathParameters(
+                        RequestDocumentation.parameterWithName("first").description("First parameter"),
+                        RequestDocumentation.parameterWithName("second").description("Second parameter")
+                )));
+    }
+
+    static class UserInput {
+
+        @NotNull
+        @Size(min = 1)
+        String name;
+
+        @NotNull
+        @Size(min = 8)
+        String password;
+
+    }
+
+    @Test
+    public void testConstrainedFields() throws Exception {
+        // see https://github.com/spring-projects/spring-restdocs/blob/v1.2.1.RELEASE/samples/rest-notes-spring-hateoas/src/test/java/com/example/notes/ApiDocumentation.java
+        final ConstraintDescriptions userConstraints = new ConstraintDescriptions(UserInput.class);
+        final List<String> descriptions = userConstraints.descriptionsForProperty("name");
+        System.out.println(descriptions);
     }
 }
